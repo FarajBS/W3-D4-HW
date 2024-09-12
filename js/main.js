29e07ec7e206ce85005652f1d8021e678b0b99fb
test <test@example.com>
2024-09-12 07:08:47 +0300

    W3-D4-HW

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let availableImg = document.getElementById("AvailableImg");
+
+availableImg.classList.add("hover");
+
+availableImg
+
+let nameJob = document.getElementById("nameJob");
+
+// nameJob.style.color = "blue";
+
+nameJob.classList.add("hover");
+
+var css = 'h1:hover{ background-color: #0000ff }';
+console.log(css);
+
